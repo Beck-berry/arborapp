dbf7b1e581d5ebea52c418597e8077faa066398b
Beck-berry <trebeka98@gmail.com>
2022-09-03 20:05:50 +0200

    update enums

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -7,26 +7,53 @@ enum LoginState {
   loggedIn,
 }
 
-enum NoteState {
-  show,
-  write,
-  modify
+enum NoteState { show, write, modify }
+
+enum AlkalmazasLehetoseg { parkfa, bokorfa, sorfa }
+
+extension AlkalmazasLehetosegExt on AlkalmazasLehetoseg {
+  String get name {
+    switch (this) {
+      case AlkalmazasLehetoseg.parkfa:
+        return 'parkfa';
+      case AlkalmazasLehetoseg.bokorfa:
+        return 'bokorfa';
+      case AlkalmazasLehetoseg.sorfa:
+        return 'sorfa';
+    }
+  }
 }
 
-enum NovenyTipus {
-  fenyo,
-  lombosFa
+enum Diszitoertek { lomb, virag, termes, level, kereg }
+
+extension DiszitoertekExt on Diszitoertek {
+  String get name {
+    switch (this) {
+      case Diszitoertek.termes:
+        return "termés";
+      case Diszitoertek.lomb:
+        return "lomb";
+      case Diszitoertek.virag:
+        return "virág";
+      case Diszitoertek.level:
+        return "levél";
+      case Diszitoertek.kereg:
+        return "kéreg";
+    }
+  }
 }
+
+enum NovenyTipus { fenyo, lombosFa, cserje }
 
 extension NovenyTipusExt on NovenyTipus {
   String get name {
     switch (this) {
       case NovenyTipus.fenyo:
-        return 'fenyő';
+        return 'tű- és pikkelylevelű';
       case NovenyTipus.lombosFa:
         return 'lombos fa';
-      default:
-        return '';
+      case NovenyTipus.cserje:
+        return 'cserje';
     }
   }
 
@@ -36,7 +63,7 @@ extension NovenyTipusExt on NovenyTipus {
         return const Color(0xFF008000);
       case NovenyTipus.lombosFa:
         return const Color(0xFF32CD32);
-      default:
+      case NovenyTipus.cserje:
         return const Color(0xFFDC143C);
     }
   }
